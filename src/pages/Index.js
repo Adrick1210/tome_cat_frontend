50bd6abf24c4d996278aa3ef8e73b6ca91ac3cec
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-24 13:39:51 -0800

    modal time

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,21 +1,42 @@
 import { useLoaderData, Link, Form } from "react-router-dom";
+import { useState } from "react";
+import Modal from "../components/Modal";
+
 
 function Index() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const books = useLoaderData();
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
-      <Form>
-        <input type="text" name="title" placeholder="New Title" />
-        <input type="text" name="author" placeholder="Author Name" />
-        <input type="checkbox" name="read" />
-        <input type="submit" value={"Create Book"} />
-      </Form>
+      <div className="create-modal">
+        <button onClick={handleOpenModal}>Add Book</button>
+        {isModalOpen && (
+          <Modal>
+            <h2>Create a Book</h2>
+            <Form>
+              <input type="text" name="title" placeholder="New Title" />
+              <input type="text" name="author" placeholder="Author Name" />
+              <input type="checkbox" name="read" />
+              <input type="submit" value={"Create Book"} />
+            </Form>
+            <button onClick={handleCloseModal}>Close Form</button>
+          </Modal>
+        )}
+      </div>
 
       <div className="list-container">
         {books.map((items) => {
           return (
-            <div className="list-item">
+            <div className="list-item" key={items._id}>
               <Link to={items._id}>
                 <h1>{items.title}</h1>
               </Link>
