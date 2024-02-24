cdfe665569170efd0281a9f6dd57a0472cce6e85
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-24 14:01:57 -0800

    update modal done

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,29 +1,49 @@
 import { useLoaderData, Form } from "react-router-dom";
+import { useState } from "react";
+import Modal from "../components/Modal";
 
 function Show() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const book = useLoaderData();
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="container">
-
       <div className="book-card">
         <h1>{book.title}</h1>
         <h2>{book.author}</h2>
         <h3>{JSON.stringify(book.read)}</h3>
       </div>
 
-      <Form action={`/update/${book._id}`} method="post">
-        <input type="text" name="title" placeholder={book.title} />
-        <input type="text" name="author" placeholder={book.author} />
-        <input type="checkbox" name="read" />
-        <input type="submit" value="book update"/>
-      </Form>
+      <div className="update-modal">
+        <button onClick={handleOpenModal}>Update</button>
+        {isModalOpen && (
+          <Modal>
+            <h2>Update {book.title}</h2>
+            <Form action={`/update/${book._id}`} method="post">
+              <input type="text" name="title" placeholder={book.title} />
+              <input type="text" name="author" placeholder={book.author} />
+              <input type="checkbox" name="read" />
+              <input type="submit" value="book update" />
+            </Form>
+          </Modal>
+        )}
+      </div>
 
       <div className="delete-button">
-        <Form action={`/delete/${book._id}`} method="post"> 
-          <button className="delete-button" type="submit">{`Delete ${book.title}`}</button>
+        <Form action={`/delete/${book._id}`} method="post">
+          <button
+            className="delete-button"
+            type="submit"
+          >{`Delete ${book.title}`}</button>
         </Form>
-
       </div>
     </div>
   );
