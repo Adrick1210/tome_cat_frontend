9ab437246d42f428bbc1a515e3e89bd1bb7b7cd2
Jason Kulak <JasonKulak@yahoo.com>
2024-02-25 11:12:26 -0600

    footer and about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function Footer() {
+
+  function handleClick() {
+    window.location.href = "/about";
+  }
     return (
       <div>
+        <button onClick={handleClick}>
+          About the Devs
+        </button>
         Footer
       </div>
     )
   }
+  
   export default Footer;
